4afdcbf0fc5c6f41caa2e5fed1f91e3a767afdda
megamos <mm_swedev@gmail.com>
2017-02-20 03:36:21 +0100

    Added neat plugin for compass

--- kodkollektivet/static/css/compass/config.rb
+++ kodkollektivet/static/css/compass/config.rb
@@ -1,6 +1,6 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
-
+require 'neat-compass'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = ".."
