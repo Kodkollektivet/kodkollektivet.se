e71437777fae8814411a5cac5f89568fa4643d22
John Herrlin <jherrlin@gmail.com>
2018-04-01 14:45:44 +0200

    Docker command updated

--- settings/settings.py
+++ settings/settings.py
@@ -119,10 +119,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-LOCALE_PATHS = (
-    os.path.join(BASE_DIR, "locale"),
-)
-
 LANGUAGES = (
     ('en', _('English')),
     ('sv', _('Swedish'))
