57ee99958e318b7f74be941165c4a4671d00f9be
John Herrlin <jherrlin@gmail.com>
2018-04-01 13:14:28 +0200

    Restructure: templates folder moved into app

--- settings/settings.py
+++ settings/settings.py
@@ -64,9 +64,7 @@ ROOT_URLCONF = 'settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, "templates"),
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
