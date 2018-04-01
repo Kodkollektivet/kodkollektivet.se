58292ccd9512c5b06ed49f384b35730c467666ae
John Herrlin <jherrlin@gmail.com>
2018-04-01 13:25:05 +0200

    Restructure: tests updated to match Django 2.0

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,6 +1,6 @@
 from unittest import skip
 from django.test import TestCase, Client
-from django.core.urlresolvers import resolve
+from django.urls import reverse
 
 from kodkollektivet.models import Project, Contributor
 
@@ -15,23 +15,23 @@ class TestProjectsEndpoint(TestCase):
         self.assertEqual(req.status_code, 200)
 
     def test_projects_view(self):
-        route = resolve('/en/projects/')
+        route = reverse('/en/projects/')
         self.assertEqual(route.func.__name__, 'ProjectsListView')
 
     def test_projects_url_name(self):
-        route = resolve('/en/projects/')
+        route = reverse('/en/projects/')
         self.assertEqual(route.url_name, 'projectslistview')
 
     def test_projects_view_name(self):
-        route = resolve('/en/projects/')
+        route = reverse('/en/projects/')
         self.assertEqual(route.view_name, 'kodkollektivet:projectslistview')
 
     def test_projects_template(self):
-        route = resolve('/en/projects/')
+        route = reverse('/en/projects/')
         self.assertEqual(route.func.view_class.template_name, 'projects/projects.html')
 
     def test_projects_model(self):
-        route = resolve('/en/projects/')
+        route = reverse('/en/projects/')
         self.assertEqual(route.func.view_class.model.__name__, 'Project')
 
 
@@ -54,23 +54,23 @@ class TestSpecificProjectEndpoint(TestCase):
         self.assertEqual(req.status_code, 200)
 
     def test_project_view(self):
-        route = resolve('/en/project/testproject/')
+        route = reverse('/en/project/testproject/')
         self.assertEqual(route.func.__name__, 'ProjectsDetailView')
 
     def test_project_url_name(self):
-        route = resolve('/en/project/testproject/')
+        route = reverse('/en/project/testproject/')
         self.assertEqual(route.url_name, 'projectdetailview')
 
     def test_project_view_name(self):
-        route = resolve('/en/project/testproject/')
+        route = reverse('/en/project/testproject/')
         self.assertEqual(route.view_name, 'kodkollektivet:projectdetailview')
 
     def test_project_template(self):
-        route = resolve('/en/project/testproject/')
+        route = reverse('/en/project/testproject/')
         self.assertEqual(route.func.view_class.template_name, 'projects/projects_detail_view.html')
 
     def test_project_model(self):
-        route = resolve('/en/project/testproject/')
+        route = reverse('/en/project/testproject/')
         self.assertEqual(route.func.view_class.model.__name__, 'Project')
 
 
@@ -94,22 +94,22 @@ class TestContributorEndpoint(TestCase):
         self.assertEqual(req.status_code, 200)
 
     def test_contributor_view(self):
-        route = resolve('/en/contributor/test-login/')
+        route = reverse('/en/contributor/test-login/')
         self.assertEqual(route.func.__name__, 'ContributorDetailView')
 
     def test_contributor_url_name(self):
-        route = resolve('/en/contributor/test-login/')
+        route = reverse('/en/contributor/test-login/')
         self.assertEqual(route.url_name, 'contributordetailview')
 
     def test_contributor_view_name(self):
-        route = resolve('/en/contributor/test-login/')
+        route = reverse('/en/contributor/test-login/')
         self.assertEqual(route.view_name, 'kodkollektivet:contributordetailview')
 
     def test_contributor_template(self):
-        route = resolve('/en/contributor/test-login/')
+        route = reverse('/en/contributor/test-login/')
         self.assertEqual(route.func.view_class.template_name,
                          'projects/contributor_detail_view.html')
 
     def test_contributor_model(self):
-        route = resolve('/en/contributor/test-login/')
+        route = reverse('/en/contributor/test-login/')
         self.assertEqual(route.func.view_class.model.__name__, 'Contributor')
